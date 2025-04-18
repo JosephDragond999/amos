2aaa396d83d093ae033ea7cec1f2237d64354811
josephdragond999 <josephdragond999@gmail.com>
2025-04-18 10:07:11 -0500

    listo

--- testComection.js
+++
@@ -1,9 +0,0 @@
-const db = require('./config/db');
-(async () => {
-    try {
-        const result = await db.query('SELECT  NOW()');
-        console.log('Conexión exitosa a la fecha y hora actual:', result.rows[0]);
-    } catch (error) {
-        console.error('Error de conexión:', error);
-    }
-})();
